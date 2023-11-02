6b46c8217adc5c033b6dd9519b489e97bab435be
Betül <kbetul@sabanciuniv.edu>
2023-11-02 23:47:44 +0300

    Multiple songs can be added by uploading structured files such as csv, json

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
+import csv
+import json
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 # Flask configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:backend777@35.240.109.106/tastetwister'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Silence the deprecation warning
-app.config['SECRET_KEY'] = 'your_secret_key_here'  # For session management, replace with a strong key
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Folder to save uploaded files
+ALLOWED_EXTENSIONS = {'csv', 'json'}
 
 db = SQLAlchemy(app)
 
@@ -29,6 +35,11 @@ class Song(db.Model):
         self.rating = new_rating
         db.session.commit()
 
+@app.route('/')
+def home():
+    if 'username' in session:
+        return redirect(url_for('songs'))
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -41,7 +52,6 @@ def register():
         flash('Registration successful! Please login.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -57,13 +67,11 @@ def login():
             flash('Login failed! Check your credentials.', 'danger')
     return render_template('login.html')
 
-
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     flash('You have been logged out.', 'success')
     return redirect(url_for('login'))
-
 
 @app.route('/songs', methods=['GET', 'POST'])
 def songs():
@@ -110,6 +118,65 @@ def songs():
     else:
         return render_template('index.html', songs=songs)
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def add_songs_from_csv(file_path, username):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            song = Song(track_name=row['track_name'], performer=row['performer'], album=row['album'], rating=int(row['rating']), username=username)
+            db.session.add(song)
+        db.session.commit()
+
+def add_songs_from_json(file_path, username):
+    with open(file_path) as jsonfile:
+        data = json.load(jsonfile)
+        for item in data:
+            try:
+                track_name = item['track_name']
+                performer = item['performer']  # this line may raise the KeyError
+                album = item['album']
+                rating = int(item['rating'])
+            except KeyError as e:
+                print(f"KeyError: {e} not found in item: {item}")
+                continue  # skips the current item and moves to the next
+
+            song = Song(track_name=track_name, performer=performer, album=album, rating=rating, username=username)
+            db.session.add(song)
+    db.session.commit()
+
+@app.route('/upload_songs', methods=['POST'])
+def upload_songs():
+    if 'username' not in session:
+        flash('Please log in first.', 'danger')
+        return redirect(url_for('login'))
+
+    if 'file' not in request.files:
+        flash('No file part', 'danger')
+        return redirect(url_for('songs'))
+    
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file', 'danger')
+        return redirect(url_for('songs'))
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        extension = filename.rsplit('.', 1)[1].lower()
+        
+        if extension == "csv":
+            add_songs_from_csv(file_path, session['username'])
+        elif extension == "json":
+            add_songs_from_json(file_path, session['username'])
+
+        flash('Songs have been uploaded successfully!', 'success')
+    else:
+        flash('Unsupported file type', 'danger')
+
+    return redirect(url_for('songs'))
 
 @app.route('/songs/<int:id>/update', methods=['POST'])
 def update_song_rating(id):
@@ -130,11 +197,11 @@ def delete_song(id):
         return redirect(url_for('songs'))
     return "Error deleting song", 404
 
-
-
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
 
+# This section will create all tables in the database if they don't exist.
 with app.app_context():
     db.create_all()
-
